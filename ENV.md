9dee644ed720513d12165cb1df0ccc7ad642dfd8
Ashwin <ashwin@micropyramid.com>
2021-12-08 17:31:13 +0530

    changed header vaiable name

--- ENV.md
+++ ENV.md
@@ -1,6 +1,6 @@
 ENV variables needed to run this app.  
 Fill the values and keep in .env file of the root folder.  
 
-MONGO_URL="your mongodb url"
-APP_ID = "random key"
-APP_SECRET = "random key"
+MONGO_URL="your mongodb url"  
+APP_ID = "random key"  
+APP_SECRET = "random key"  
--- middleware/checkCredentials.js
+++ middleware/checkCredentials.js
@@ -1,12 +1,10 @@
 const { request } = require("express");
 
 const checkCredentials = (req, res, next) => {
-  console.log(req.headers['app_secret']);
   const app_id = process.env.APP_ID;
   const app_secret = process.env.APP_SECRET;
 
-  console.log(app_id);
-  if (req.headers['app_id']==app_id && req.headers['app_secret']==app_secret){
+  if (req.headers['app-id']==app_id && req.headers['app-secret']==app_secret){
     next();
   }
   else{
